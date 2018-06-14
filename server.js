f7b5c9c0db8ec58e8ab1877124a4f050dbd2c2a0
Felipebr26 <fcamparasilva@gmail.com>
2018-06-14 08:42:12 -0300

    https

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import serveStatic from 'serve-static'
 import history from 'connect-history-api-fallback'
-import secure from 'ssl-express-www'
+import enforce from 'express-sslify'
 
 const app = express()
 const port = process.env.PORT || 5000
 
-app.use(secure)
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
 app.use(history())
 app.use(serveStatic(`${__dirname}/dist/pwa-mat`))
 app.listen(port)
