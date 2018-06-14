08d97288e6a8fb6f4c61696933d356e3c20bd3cd
Felipebr26 <fcamparasilva@gmail.com>
2018-06-14 08:51:11 -0300

    remove https

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 import express from 'express'
 import serveStatic from 'serve-static'
 import history from 'connect-history-api-fallback'
-import enforce from 'express-sslify'
 
 const app = express()
 const port = process.env.PORT || 5000
 
-app.use(enforce.HTTPS({ trustProtoHeader: true }))
 app.use(history())
 app.use(serveStatic(`${__dirname}/dist/pwa-mat`))
 app.listen(port)
