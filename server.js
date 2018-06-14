5d12fabd5a273272068a2073dbd1a9de29ac56dd
Felipebr26 <fcamparasilva@gmail.com>
2018-06-14 08:56:11 -0300

    rollback

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-import express from 'express'
-import serveStatic from 'serve-static'
-import history from 'connect-history-api-fallback'
+const
+  express = require('express'),
+  serveStatic = require('serve-static'),
+  history = require('connect-history-api-fallback'),
+  port = process.env.PORT || 5000
 
 const app = express()
-const port = process.env.PORT || 5000
 
 app.use(history())
-app.use(serveStatic(`${__dirname}/dist/pwa-mat`))
+app.use(serveStatic(`${__dirname}/dist/spa-<theme>`))
 app.listen(port)
--- src/store/auth/index.js
+++ src/store/auth/index.js
@@ -1,4 +1,4 @@
-import Firebase from 'firebase'
+import Firebase from 'firebase/app'
 
 export default {
   namespaced: true,
